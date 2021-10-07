60b4a702a57f6d3b15a197410784da41ee97e54a
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-10-07 17:16:10 -0300

    created TransactioTable component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TransactionTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Summary />;
+      <Summary />
+      <TransactionTable />
     </Container>
   );
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,4 +1,6 @@
 import incomeImg from "../../assets/entradas.svg";
+import outcomeImg from "../../assets/saídas.svg";
+import totalImg from "../../assets/total.svg";
 import { Container } from "./styles";
 
 export function Summary() {
@@ -15,17 +17,17 @@ export function Summary() {
       <div>
         <header>
           <p>Saídas</p>
-          <img src={incomeImg} alt="" />
+          <img src={outcomeImg} alt="" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong> - R$500,00</strong>
       </div>
 
       <div className="highlight-background">
         <header>
           <p>Total</p>
-          <img src={incomeImg} alt="" />
+          <img src={totalImg} alt="" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>R$500,00</strong>
       </div>
     </Container>
   );

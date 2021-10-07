5d9e82b6aef2ade295018efbc45db3d97b618411
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2021-10-07 16:44:30 -0300

    created dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { Summary } from "./components/Summary";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <>
       <Header />
-      <Summary />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
